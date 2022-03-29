763ceae8ca46c40381cb9beb0cdeee502aa6990d
Amir Bashan <90416342+amirbashan@users.noreply.github.com>
2022-03-29 14:57:41 +0300

    scroll to about

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Welcome from "./Components/Welcome";
+import AboutMe from "./Components/AboutMe";
 
 function App() {
+  const aboutRef = useRef();
+
+  const handleBackClick = () => {
+    aboutRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <ChakraProvider>
       <div>
         <div id="welcome">
-          <Welcome />
+          <Welcome handleBackClick={handleBackClick} />
         </div>
-        <div id="about"></div>
+        <div id="about" ref={aboutRef}>
+          <AboutMe />
+        </div>
         <div id="projects"></div>
         <div id="contact"></div>
       </div>
--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import { Heading } from "@chakra-ui/react";
 
-export default function Welcome() {
+export default function Welcome(props) {
+  const { handleBackClick } = props;
   return (
     <div className="welcomeContainer">
       <Heading size="4xl">
         Hi, my name is <span>Amir Bashan</span>
       </Heading>
       <Heading size="4xl">I'm a Full stack developer</Heading>
-      <Button variant="outline-success" size="lg">
+      <Button variant="outline-success" size="lg" onClick={handleBackClick}>
         See more
       </Button>
     </div>
