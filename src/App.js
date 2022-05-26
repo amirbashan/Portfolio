5ff68cfd41a7e4f2325a33d62a357aa53735a53c
Amir Bashan <90416342+amirbashan@users.noreply.github.com>
2022-05-26 18:35:08 +0300

    added buildpack

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useRef } from "react";
-import { ChakraProvider } from "@chakra-ui/react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import Navbar from "./Components/Navbar";
-import Welcome from "./Components/Welcome";
-import AboutMe from "./Components/AboutMe";
-import Skills from "./Components/Skills";
-import Projects from "./Components/Projects";
-import ContactMe from "./Components/ContactMe";
-import whatsapp from "./Img/whatsapp.png";
+import React, { useEffect, useRef } from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Navbar from './Components/Navbar';
+import Welcome from './Components/Welcome';
+import AboutMe from './Components/AboutMe';
+import Skills from './Components/Skills';
+import Projects from './Components/Projects';
+import ContactMe from './Components/ContactMe';
+import whatsapp from './Img/whatsapp.png';
 
-import Aos from "aos";
-import "aos/dist/aos.css";
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 function App() {
   const skillsRef = useRef();
@@ -23,16 +23,16 @@ function App() {
     Aos.init({});
   }, []);
 
-  const ClickSkills = () => skillsRef.current.scrollIntoView({ behavior: "smooth" });
-  const ClickAbout = () => aboutRef.current.scrollIntoView({ behavior: "smooth" });
-  const ClickProjects = () => projectsRef.current.scrollIntoView({ behavior: "smooth" });
-  const ClickContact = () => contactRef.current.scrollIntoView({ behavior: "smooth" });
+  const ClickSkills = () => skillsRef.current.scrollIntoView({ behavior: 'smooth' });
+  const ClickAbout = () => aboutRef.current.scrollIntoView({ behavior: 'smooth' });
+  const ClickProjects = () => projectsRef.current.scrollIntoView({ behavior: 'smooth' });
+  const ClickContact = () => contactRef.current.scrollIntoView({ behavior: 'smooth' });
 
   return (
     <ChakraProvider>
       <div>
         <Navbar />
-        <div id="welcome">
+        <div id='welcome'>
           <Welcome
             ClickAbout={ClickAbout}
             ClickSkills={ClickSkills}
@@ -40,27 +40,27 @@ function App() {
             ClickContact={ClickContact}
           />
         </div>
-        <div className="emptySpace" ref={aboutRef}></div>
-        <div id="about">
+        <div className='emptySpace' ref={aboutRef}></div>
+        <div id='about'>
           <AboutMe />
         </div>
-        <div className="emptySpace"></div>
-        <div id="projects" ref={projectsRef}>
+        <div className='emptySpace'></div>
+        <div id='projects' ref={projectsRef}>
           <Projects />
         </div>
-        <div className="emptySpace" ref={skillsRef}></div>
-        <div id="techSkills">
+        <div className='emptySpace' ref={skillsRef}></div>
+        <div id='techSkills'>
           <Skills />
         </div>
-        <div id="contact" ref={contactRef}>
+        <div id='contact' ref={contactRef}>
           <ContactMe />
         </div>
         <footer>
-          <img src={whatsapp} title="whatsapp" alt={"whatsapp link"} />
-          <a target="_blank" href="https://wa.me/0585009044">
+          <img src={whatsapp} title='whatsapp' alt={'whatsapp link'} />
+          <a target='_blank' href='https://wa.me/0585009044'>
             Amir 0585-009044
           </a>
-          <img src={whatsapp} title="whatsapp" alt={"whatsapp link"} />
+          <img src={whatsapp} title='whatsapp' alt={'whatsapp link'} />
         </footer>
       </div>
     </ChakraProvider>
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,13 +1,12 @@
-import React from "react";
-import { Heading } from "@chakra-ui/react";
-import ProjectDeltika from "./ProjectDeltika";
-import ProjectArcade from "./ProjectArcade";
+import React from 'react';
+import { Heading } from '@chakra-ui/react';
+import ProjectDeltika from './ProjectDeltika';
+import ProjectArcade from './ProjectArcade';
 
 export default function Projects() {
-
   return (
     <>
-      <Heading size="2xl" className="mb-3">
+      <Heading size='2xl' className='mb-3'>
         Projects
       </Heading>
       <ProjectDeltika />
