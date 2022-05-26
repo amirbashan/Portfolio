f7740ca70e1f6eb35a1d84b2a46e9294aee32ee3
Amir Bashan <90416342+amirbashan@users.noreply.github.com>
2022-05-26 17:00:04 +0300

    make it run faster

--- src/Components/ProjectArcade.jsx
+++ src/Components/ProjectArcade.jsx
@@ -1,44 +1,44 @@
-import React from "react";
-import { Heading } from "@chakra-ui/react";
-import oldArcade from "../Vid/oldArcade.mp4";
+import React from 'react';
+import { Heading } from '@chakra-ui/react';
 
 export default function ProjectArcade() {
   return (
-    <div className="projectFrame">
-      <div data-aos="fade-right" className="projectInfo">
-        <Heading size="lg" className="mb-1">
+    <div className='projectFrame'>
+      <div data-aos='fade-right' className='projectInfo'>
+        <Heading size='lg' className='mb-1'>
           Retro Arcade
         </Heading>
-        <div className="skills">
-          <button className="skill btn-secondary btn-sm">React</button>
-          <button className="skill btn-secondary btn-sm">React-Router</button>
-          <button className="skill btn-secondary btn-sm">Node-js</button>
-          <button className="skill btn-secondary btn-sm">Express</button>
-          <button className="skill btn-secondary btn-sm">MySQL</button>
+        <div className='skills'>
+          <button className='skill btn-secondary btn-sm'>React</button>
+          <button className='skill btn-secondary btn-sm'>React-Router</button>
+          <button className='skill btn-secondary btn-sm'>Node-js</button>
+          <button className='skill btn-secondary btn-sm'>Express</button>
+          <button className='skill btn-secondary btn-sm'>MySQL</button>
         </div>
         <div>
-          {/* <p>
-            <b>Collaborator:</b> Shahar Knafo.
-          </p> */}
           <p>This project was done as a part of our ITC coding Bootcamp.</p>
           <p>We coded website, games logic ,created and connected to a users & scores database</p>
-          <p className="note">
+          <p className='note'>
             <b>Note:</b> All work was done as a 1-week exercise.
           </p>
         </div>
-        <div className="buttonStyle">
-          <a target="_blank" href="https://retro-arcade.herokuapp.com/">
+        <div className='buttonStyle'>
+          <a target='_blank' href='https://retro-arcade.herokuapp.com/'>
             See Live
           </a>
-          <a target="_blank" href="https://github.com/amirbashan/oldArcade">
+          <a target='_blank' href='https://github.com/amirbashan/oldArcade'>
             Source Code
           </a>
         </div>
       </div>
-      <div data-aos="fade-left" className="projectImg">
-        <video className="video" loop={true} autoPlay={true} muted={true}>
-          <source src={oldArcade} type="video/mp4" />
-        </video>
+      <div data-aos='fade-left' className='projectView'>
+        <iframe
+          width='100%'
+          height='315'
+          src='https://www.youtube.com/embed/zub5VDROQVI'
+          frameborder='0'
+          allowfullscreen='true'
+        ></iframe>
       </div>
     </div>
   );
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,97 +1,142 @@
-import React from "react";
-import { Heading } from "@chakra-ui/react";
-import html5 from "../Img/Logo/HTML5.png";
-import css3 from "../Img/Logo/CSS3.png";
-import JavaScript from "../Img/Logo/JavaScript.png";
-import reactIcon from "../Img/Logo/reactIcon.png";
-import mapbox from "../Img/Logo/mapbox.png";
-import BootstrapIcon from "../Img/Logo/Bootstrap.png";
-import nodejsIcon from "../Img/Logo/nodejs.png";
-import mysql from "../Img/Logo/mysql.png";
-import postgresql from "../Img/Logo/postgresql.png";
-import firebase from "../Img/Logo/firebase.png";
-import mongodb from "../Img/Logo/mongodb.png";
-import express from "../Img/Logo/express.png";
-import github from "../Img/Logo/github.png";
-import jira from "../Img/Logo/jira.png";
-import heroku from "../Img/Logo/heroku.png";
+import React from 'react';
+import { Heading } from '@chakra-ui/react';
+// import html5 from "../Img/Logo/HTML5.png";
+// import css3 from "../Img/Logo/CSS3.png";
+// import JavaScript from "../Img/Logo/JavaScript.png";
+// import reactIcon from "../Img/Logo/reactIcon.png";
+// import mapbox from "../Img/Logo/mapbox.png";
+// import BootstrapIcon from "../Img/Logo/Bootstrap.png";
+// import nodejsIcon from "../Img/Logo/nodejs.png";
+// import mysql from "../Img/Logo/mysql.png";
+// import postgresql from "../Img/Logo/postgresql.png";
+// import firebase from "../Img/Logo/firebase.png";
+// import mongodb from "../Img/Logo/mongodb.png";
+// import express from "../Img/Logo/express.png";
+// import github from "../Img/Logo/github.png";
+// import jira from "../Img/Logo/jira.png";
+// import heroku from "../Img/Logo/heroku.png";
 
 export default function Skills() {
   return (
     <>
-      <Heading size="2xl">SKILLS</Heading>
-      <div className="SkillsContainer">
-        <div id="skillsFront" className="SkillsInner">
-          <Heading size="md">Front-End</Heading>
-          <div className="miniSkillsContainer">
-            <div className="miniSkill">
-              <img src={html5} alt={"HTML5"} />
+      <Heading size='2xl'>SKILLS</Heading>
+      <div className='SkillsContainer'>
+        <div id='skillsFront' className='SkillsInner'>
+          <Heading size='md'>Front-End</Heading>
+          <div className='miniSkillsContainer'>
+            <div className='miniSkill'>
+              <img
+                src='https://res.cloudinary.com/amirbashan/image/upload/v1653573182/Portfolio/HTML5_nl78nc.png'
+                alt={'HTML5'}
+              />
               <p>HTML5</p>
             </div>
-            <div className="miniSkill">
-              <img src={css3} alt={"CSS3"} />
+            <div className='miniSkill'>
+              <img
+                src='https://res.cloudinary.com/amirbashan/image/upload/v1653573182/Portfolio/CSS3_efsmx0.png'
+                alt={'CSS3'}
+              />
               <p>CSS3</p>
             </div>
-            <div className="miniSkill">
-              <img src={JavaScript} alt={"JavaScript"} />
+            <div className='miniSkill'>
+              <img
+                src='https://res.cloudinary.com/amirbashan/image/upload/v1653573181/Portfolio/JavaScript_xc5pyz.png'
+                alt={'JavaScript'}
+              />
               <p>JavaScript</p>
             </div>
-            <div className="miniSkill">
-              <img src={reactIcon} alt={"React"} />
+            <div className='miniSkill'>
+              <img
+                src='https://res.cloudinary.com/amirbashan/image/upload/v1653573182/Portfolio/reactIcon_j7zdvh.png'
+                alt={'React'}
+              />
               <p>React</p>
             </div>
-            <div className="miniSkill">
-              <img src={mapbox} alt={"mapbox"} />
+            <div className='miniSkill'>
+              <img
+                src='https://res.cloudinary.com/amirbashan/image/upload/v1653573181/Portfolio/mapbox_lbtuv2.png'
+                alt={'mapbox'}
+              />
               <p>Mapbox</p>
             </div>
-            <div className="miniSkill">
-              <img src={BootstrapIcon} alt={"Bootstrap"} />
+            <div className='miniSkill'>
+              <img
+                src='https://res.cloudinary.com/amirbashan/image/upload/v1653573182/Portfolio/Bootstrap_gilfkf.png'
+                alt={'Bootstrap'}
+              />
               <p>Bootstrap</p>
             </div>
           </div>
         </div>
-        <div id="skillsBack" className="otherTools SkillsInner">
-          <Heading size="md">Other-Tools</Heading>
-          <div className="miniSkillsContainer">
-            <div className="miniSkill">
-              <img src={github} alt={"github"} />
+        <div id='skillsBack' className='otherTools SkillsInner'>
+          <Heading size='md'>Other-Tools</Heading>
+          <div className='miniSkillsContainer'>
+            <div className='miniSkill'>
+              <img
+                src='https://res.cloudinary.com/amirbashan/image/upload/v1653573182/Portfolio/github_ad8lz7.png'
+                alt={'github'}
+              />
               <p>Github</p>
             </div>
-            <div className="miniSkill">
-              <img src={jira} alt={"jira"} />
+            <div className='miniSkill'>
+              <img
+                src='https://res.cloudinary.com/amirbashan/image/upload/v1653573181/Portfolio/jira_ek60tt.png'
+                alt={'jira'}
+              />
               <p>Jira</p>
             </div>
-            <div className="miniSkill">
-              <img src={heroku} alt={"heroku"} />
+            <div className='miniSkill'>
+              <img
+                src='https://res.cloudinary.com/amirbashan/image/upload/v1653573181/Portfolio/heroku_z05jxf.png'
+                alt={'heroku'}
+              />
               <p>Heroku</p>
             </div>
           </div>
         </div>
-        <div id="skillsTools" className="SkillsInner">
-          <Heading size="md">Back-End</Heading>
-          <div className="miniSkillsContainer">
-            <div className="miniSkill">
-              <img src={nodejsIcon} alt={"nodejs"} />
+        <div id='skillsTools' className='SkillsInner'>
+          <Heading size='md'>Back-End</Heading>
+          <div className='miniSkillsContainer'>
+            <div className='miniSkill'>
+              <img
+                src='https://res.cloudinary.com/amirbashan/image/upload/v1653573181/Portfolio/nodejs_zg4rs8.png'
+                alt={'nodejs'}
+              />
               <p>nodejs</p>
             </div>
-            <div className="miniSkill">
-              <img src={mysql} alt={"mysql"} />
+            <div className='miniSkill'>
+              <img
+                src='https://res.cloudinary.com/amirbashan/image/upload/v1653573181/Portfolio/mysql_q4brqh.png'
+                alt={'mysql'}
+              />
               <p>MySQL</p>
             </div>
-            <div className="miniSkill">
-              <img src={postgresql} alt={"postgresql"} />
+            <div className='miniSkill'>
+              <img
+                src='https://res.cloudinary.com/amirbashan/image/upload/v1653573182/Portfolio/postgresql_uimbgt.png'
+                alt={'postgresql'}
+              />
               <p>PostgreSQL</p>
             </div>
-            <div className="miniSkill">
-              <img src={firebase} alt={"firebase"} />
+            <div className='miniSkill'>
+              <img
+                src='https://res.cloudinary.com/amirbashan/image/upload/v1653573182/Portfolio/firebase_bbcqiq.png'
+                alt={'firebase'}
+              />
               <p>Firebase</p>
             </div>
-            <div className="miniSkill">
-              <img src={mongodb} alt={"mongodb"} />
+            <div className='miniSkill'>
+              <img
+                src='https://res.cloudinary.com/amirbashan/image/upload/v1653573181/Portfolio/mongodb_q0emy8.png'
+                alt={'mongodb'}
+              />
               <p>Mongodb</p>
             </div>
-            <div className="miniSkill">
-              <img src={express} alt={"express"} />
+            <div className='miniSkill'>
+              <img
+                src='https://res.cloudinary.com/amirbashan/image/upload/v1653573182/Portfolio/express_cgl7wp.png'
+                alt={'express'}
+              />
               <p>Express</p>
             </div>
           </div>
