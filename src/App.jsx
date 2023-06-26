2a58f8c1df1ddce25fd6f21025124efe20bd233e
Amir Bashan <90416342+amirbashan@users.noreply.github.com>
2023-06-26 21:19:43 +0300

    change to Vite (#1)
    * change to vite
    * fixed dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import AboutMe from './Components/AboutMe';
 import Skills from './Components/Skills';
 import Projects from './Components/Projects';
 import ContactMe from './Components/ContactMe';
-import whatsapp from './Img/whatsapp.png';
 
 import Aos from 'aos';
 import 'aos/dist/aos.css';
@@ -23,16 +22,20 @@ function App() {
     Aos.init({});
   }, []);
 
-  const ClickSkills = () => skillsRef.current.scrollIntoView({ behavior: 'smooth' });
-  const ClickAbout = () => aboutRef.current.scrollIntoView({ behavior: 'smooth' });
-  const ClickProjects = () => projectsRef.current.scrollIntoView({ behavior: 'smooth' });
-  const ClickContact = () => contactRef.current.scrollIntoView({ behavior: 'smooth' });
+  const ClickSkills = () =>
+    skillsRef.current.scrollIntoView({ behavior: 'smooth' });
+  const ClickAbout = () =>
+    aboutRef.current.scrollIntoView({ behavior: 'smooth' });
+  const ClickProjects = () =>
+    projectsRef.current.scrollIntoView({ behavior: 'smooth' });
+  const ClickContact = () =>
+    contactRef.current.scrollIntoView({ behavior: 'smooth' });
 
   return (
     <ChakraProvider>
       <div>
         <Navbar />
-        <div id='welcome'>
+        <div id="welcome">
           <Welcome
             ClickAbout={ClickAbout}
             ClickSkills={ClickSkills}
@@ -40,28 +43,21 @@ function App() {
             ClickContact={ClickContact}
           />
         </div>
-        <div className='emptySpace' ref={aboutRef}></div>
-        <div id='about'>
+        <div className="emptySpace" ref={aboutRef}></div>
+        <div id="about">
           <AboutMe />
         </div>
-        <div className='emptySpace'></div>
-        <div id='projects' ref={projectsRef}>
+        <div className="emptySpace"></div>
+        <div id="projects" ref={projectsRef}>
           <Projects />
         </div>
-        <div className='emptySpace' ref={skillsRef}></div>
-        <div id='techSkills'>
+        <div className="emptySpace" ref={skillsRef}></div>
+        <div id="techSkills">
           <Skills />
         </div>
-        <div id='contact' ref={contactRef}>
+        <div id="contact" ref={contactRef}>
           <ContactMe />
         </div>
-        <footer>
-          <img src={whatsapp} title='whatsapp' alt={'whatsapp link'} />
-          <a target='_blank' href='https://wa.me/0585009044'>
-            Amir 0585-009044
-          </a>
-          <img src={whatsapp} title='whatsapp' alt={'whatsapp link'} />
-        </footer>
       </div>
     </ChakraProvider>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,25 @@
-import React from "react";
-import resume from "../Img/resume.png";
-import gitIcon from "../Img/github.png";
-import linkedinIcon from "../Img/linkedin.png";
+import React from 'react';
+import resume from '../assets/resume.png';
+import gitIcon from '../assets/github.png';
+import linkedinIcon from '../assets/linkedin.png';
 
 export default function Navbar() {
   return (
     <div id="navbar">
-      <a target="_blank" href="https://drive.google.com/file/d/1ixxJBlCQJKo3z34rxlIEwH34bbFEIqD9/view?usp=sharing">
-        <img src={resume} alt={"My Resume"} title="My Resume" />
+      <a
+        target="_blank"
+        href="https://drive.google.com/file/d/1ixxJBlCQJKo3z34rxlIEwH34bbFEIqD9/view?usp=sharing"
+      >
+        <img src={resume} alt={'My Resume'} title="My Resume" />
       </a>
-      <a target="_blank" href="https://www.linkedin.com/in/amir-bashan-2a48a8124/">
-        <img src={linkedinIcon} title="linkedin" alt={"linkedin link"} />
+      <a
+        target="_blank"
+        href="https://www.linkedin.com/in/amir-bashan-2a48a8124/"
+      >
+        <img src={linkedinIcon} title="linkedin" alt={'linkedin link'} />
       </a>
       <a target="_blank" href="https://github.com/amirbashan">
-        <img src={gitIcon} alt={"github link"} title="github" />
+        <img src={gitIcon} alt={'github link'} title="github" />
       </a>
     </div>
   );
